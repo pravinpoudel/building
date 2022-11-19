73ae916278345a2257eb6b10b96ac2b645661fd4
pravinpoudel <prvnpoudel4@gmail.com>
2022-11-19 16:31:15 -0700

    temporary trying with the DNA displacement data

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { Color, FloatType, Scene, Vec2 } from 'three'
+import { Color, FloatType, Mesh, Scene, Vec2 } from 'three'
 import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls'
 import { DragControls } from 'three/examples/jsm/controls/DragControls'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
@@ -278,47 +278,42 @@ function init() {
     document.body.appendChild(renderer.domElement)
 
     document.addEventListener('mousemove', onPointerMove)
-
+    const displacementMap = new THREE.TextureLoader().load('./models/DNA/displacement-texture.jpg')
+    let _material = new THREE.MeshStandardMaterial({
+        color: 0x880000,
+        aoMapIntensity: 1,
+        displacementMap: displacementMap,
+        displacementScale: 1,
+        displacementBias: -0.428408, // from original model
+        side: THREE.DoubleSide,
+    })
     async function load_model() {
         const gltfLoader = new GLTFLoader()
         gltfLoader.setDRACOLoader(dracoLoader)
-        await gltfLoader
-            .setPath('./models/drawing_room/')
-            .load('scene-merged-draco.glb', function (gltf) {
-                console.log(gltf.scene.children[0])
-                const modifier = new SimplifyModifier()
-                gltf.scene.traverse(function (child) {
-                    if (child instanceof THREE.Mesh) {
-                        const _child = child as THREE.Mesh
-
-                        // computing bounding box for it's geometry
-                        // we only have to compute it's bounding box because this is static mesh
-                        _child.geometry.computeBoundingBox() //AABB
-                        _child.castShadow = true
-                        _child.receiveShadow = true
-                        _child.scale.set(100, 100, 100)
-                        sceneObjects.push(child)
-                        // let verticesToRemove = Math.floor(
-                        //     _child.geometry.attributes.position.count * 0.1
-                        // )
-                        // _child.geometry = modifier.modify(_child.geometry, verticesToRemove)
-                    }
-                    if (child instanceof THREE.Light) {
-                        const _light = child as THREE.Light
-                        _light.castShadow = true
-                        _light.shadow.bias = 0.0008 // to reduce artifact in shadow
-                        _light.shadow.mapSize.width = 1024
-                        _light.shadow.mapSize.height = 1024
-                    }
-                })
-                gltf.scene.translateY
-                scene.add(gltf.scene)
-                ;(document.getElementById('loader') as HTMLDivElement).style.display = 'none'
-                ;(mainscreen as HTMLElement).style.display = 'block'
-                document
-                    .getElementById('drop_test_btn')
-                    ?.addEventListener('click', drop_test_handler)
+        await gltfLoader.setPath('./models/DNA/').load('DNA-02.glb', function (gltf) {
+            gltf.scene.traverse(function (child) {
+                if (child instanceof THREE.Mesh) {
+                    const _child = child as THREE.Mesh
+                    let geometry = _child.geometry
+                    console.log(_child.material)
+                    let mesh = new THREE.Mesh(geometry, _material)
+                    mesh.scale.multiplyScalar(100)
+                    scene.add(mesh)
+                    sceneObjects.push(child)
+                }
+                if (child instanceof THREE.Light) {
+                    const _light = child as THREE.Light
+                    _light.castShadow = true
+                    _light.shadow.bias = 0.0008 // to reduce artifact in shadow
+                    _light.shadow.mapSize.width = 1024
+                    _light.shadow.mapSize.height = 1024
+                }
             })
+            console.log(scene)
+            ;(document.getElementById('loader') as HTMLDivElement).style.display = 'none'
+            ;(mainscreen as HTMLElement).style.display = 'block'
+            document.getElementById('drop_test_btn')?.addEventListener('click', drop_test_handler)
+        })
     }
 
     function drop_test_handler() {
